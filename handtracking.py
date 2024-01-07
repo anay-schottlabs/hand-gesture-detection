02bb636a44cdd0902037462f58254073213283d8
anay-schottlabs <anay@schottlabs.com>
2024-01-06 18:46:12 -0800

    Added hand landmarks and connections to camera feed

--- handtracking.py
+++ handtracking.py
@@ -1,10 +1,26 @@
 import cv2
 import mediapipe
-import time
 
+# Set up camera
 capture = cv2.VideoCapture(0)
 
+# Set up hands
+mediapipeHands = mediapipe.solutions.hands
+mediapipeDraw = mediapipe.solutions.drawing_utils
+hands = mediapipeHands.Hands()
+
 while True:
+    # Get the current frame of the camera feed
     success, image = capture.read()
+    # Convert image to RGB
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # Draw landmarks on each hand in the image
+    results = hands.process(imageRGB)
+    if results.multi_hand_landmarks:
+        for handLandmark in results.multi_hand_landmarks:
+            mediapipeDraw.draw_landmarks(image, handLandmark, mediapipeHands.HAND_CONNECTIONS)
+
+    # Display the image with the hands
     cv2.imshow("Image", image)
     cv2.waitKey(1)
