9ecb85663e1fd28e3dbd1189f713a651a5d4385c
anay-schottlabs <anay@schottlabs.com>
2024-01-07 18:46:58 -0800

    Created module for recognizing gestures

--- gesturerecognition.py
+++ gesturerecognition.py
@@ -1,46 +1,41 @@
-import cv2
 import handtracking
+import json
+import fastdtw
 
-cameraWidth = 1080
-cameraHeight = 720
-loopDelay = 2
-
-capture = cv2.VideoCapture(0)
-capture.set(3, cameraWidth)
-capture.set(1, cameraHeight)
-
-while True:
-    handFinder = handtracking.handDetector(4, 0.7, 0.5)
-    image = capture.read()[1]
-    hands = handFinder.getHandLandmarksFromImage(image)
-
-    fistClenchDistanceThreshold = 20
-    indexClenched = False
-    middleClenched = False
-    ringClenched = False
-    pinkyClenched = False
-    thumbClenched = False
-    rightHand = False
-    if (hands != None):
-        for i in range(len(hands)):
-            hand = handFinder.findHandLandmarkPositions(image, i)
-            try:
-                if (hand[8]["y"] > hand[7]["y"] > hand[6]["y"]):
-                    indexClenched = True
-                if (hand[12]["y"] > hand[11]["y"] > hand[10]["y"]):
-                    middleClenched = True
-                if (hand[16]["y"] > hand[15]["y"] > hand[14]["y"]):
-                    ringClenched = True
-                if (hand[20]["y"] > hand[19]["y"] > hand[18]["y"]):
-                    pinkyClenched = True
-                if (hand[20]["x"] < hand[4]["x"]):
-                    rightHand = True
-                if (hand[3]["y"] < hand[2]["y"] and ((not rightHand and (hand[4]["x"] > hand[3]["x"])) or (rightHand and (hand[4]["x"] < hand[3]["x"])))):
-                    thumbClenched = True
-                if (indexClenched and middleClenched and ringClenched and pinkyClenched and thumbClenched):
-                    cv2.circle(image, (hand[11]["x"], hand[11]["y"]), 100, (0, 0, 255))
-            except:
-                break
-
-    cv2.imshow("Image", handFinder.detectHands(image))
-    cv2.waitKey(loopDelay)
+class GestureDetector:
+    def __init__(self, maxHands, detectionConfidence, trackingConfidence):
+        self.handDetector = handtracking.HandDetector(maxHands, detectionConfidence, trackingConfidence)
+    
+    def detectOpenPalm(self, image):
+        thumbOpen = False
+        indexOpen = False
+        middleOpen = False
+        ringOpen = False
+        pinkyOpen = False
+        hands = self.handDetector.getHandLandmarksFromImage(image)
+        if (hands):
+            for i in range(len(hands)):
+                hand = self.handDetector.findHandLandmarkPositions(image, i)
+                try:
+                    if (hand[2]["y"] > hand[3]["y"] > hand[4]["y"]):
+                        thumbOpen = True
+                    if (hand[6]["y"] > hand[7]["y"] > hand[8]["y"]):
+                        indexOpen = True
+                    if (hand[10]["y"] > hand[11]["y"] > hand[12]["y"]):
+                        middleOpen = True
+                    if (hand[14]["y"] > hand[15]["y"] > hand[16]["y"]):
+                        ringOpen = True
+                    if (hand[18]["y"] > hand[19]["y"] > hand[20]["y"]):
+                        pinkyOpen = True
+                    if (thumbOpen and indexOpen and middleOpen and ringOpen and pinkyOpen):
+                        return True
+                except:
+                    break
+            return False
+    
+    def mostSimilarGestureName(self, newGesture, existingGesturesJSON):
+        existingGestures = json.loads(existingGesturesJSON)
+        gestureCosts = []
+        for existingGesture in existingGestures:
+            gestureCosts.append(fastdtw.fastdtw(newGesture, existingGesture))
+        return existingGestures[gestureCosts.index(min(gestureCosts))]["name"]
