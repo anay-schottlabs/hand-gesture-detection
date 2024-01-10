7b6bf695b65e3490cb2a969f7601b0babd62bf6c
anay-schottlabs <anay@schottlabs.com>
2024-01-09 18:59:48 -0800

    Added method to get number of hands in image

--- handtracking.py
+++ handtracking.py
@@ -1,16 +1,18 @@
 import cv2
 import mediapipe
 
-class handDetector():
+# A class to detect, track, and mark hands in images
+class HandDetector:
     def __init__(self, maxHands, detectionConfidence, trackingConfidence):
         self.mediapipeHands = mediapipe.solutions.hands
         self.mediapipeDraw = mediapipe.solutions.drawing_utils
         self.hands = self.mediapipeHands.Hands(max_num_hands = maxHands, min_detection_confidence=detectionConfidence, min_tracking_confidence=trackingConfidence)
 
+    # Get the landmarks of hands in an image
     def getHandLandmarksFromImage(self, image):
         imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return self.hands.process(imageRGB).multi_hand_landmarks
-    
+        return self.hands.process(imageRGB).multi_hand_landmarks                                                                                                                                                      
+
     def detectHands(self, image):
         handLandmarks = self.getHandLandmarksFromImage(image)
         if handLandmarks:
@@ -18,14 +20,19 @@ class handDetector():
                 self.mediapipeDraw.draw_landmarks(image, handLandmark, self.mediapipeHands.HAND_CONNECTIONS)
         return image
     
-    def findHandLandmarkPositions(self, image, handIndex):
+    def getHandCountInImage(self, image):
+        hands = self.getHandLandmarksFromImage(image)
+        if (hands):
+            return len(hands)
+        return 0
+
+    def getHandLandmarkPositions(self, image, handIndex):
         landmarks = []
         chosenHand = self.getHandLandmarksFromImage(image)
         if chosenHand:
             chosenHand = chosenHand[handIndex]
             for _, landmark in enumerate(chosenHand.landmark):
-                width = image.shape[1]
-                height = image.shape[0]
+                height, width = image.shape;
                 xPos = int(landmark.x * width)
                 yPos = int(landmark.y * height)
                 landmarks.append({
